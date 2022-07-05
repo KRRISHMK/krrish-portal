9b1a2ce816fffeb45475dc79301fa6d49a8b5f55
muthu <muthukrrish333@gmail.com>
2022-07-06 00:06:23 +0530

    Added new cart button

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { Button } from "rsuite";
+import React, { useState } from "react";
+import { BsCart3 } from "react-icons/bs";
+import { ImGift } from "react-icons/im";
+import { MdShoppingCart } from "react-icons/md";
 
+import "./styles/cartButton.css";
 const CartButton = (props) => {
+  const [click, setClick] = useState(false);
+
+  function cartClick() {
+    setClick(true);
+  }
+
   return (
-    <div className="pb-2">
-      <Button className="w-100" appearance="primary">{props.label}</Button>
-    </div>
+    <button
+      className={`cart-button ${click ? "clicked" : ""}`}
+      onClick={() => {
+        cartClick();
+      }}
+    >
+      <span className={`${click ? "d-none" : ""} "add-to-cart fw-bold"`}>Add to Cart</span>
+      <span className="added"><MdShoppingCart /> Added</span>
+      <BsCart3 className="cart-icon" />
+      <ImGift className="cart-item" />
+    </button>
   );
 };
 
